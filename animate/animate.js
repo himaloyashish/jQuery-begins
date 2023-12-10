70769981d0d531a5e65efdaa71d60b5d48c01fb9
himaloyash <himaloyashis98@gmail.com>
2023-12-11 00:25:37 +0600

    animate done

--- animate/animate.js
+++ animate/animate.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
 
-    $("button").click(function () {
-        var ashu = $("div")
-        ashu.animate({ height:"300px", opacity:"0.4"}, "slow")
-        ashu.animate({ width:"300px", opacity:"0.5"}, "slow")
-        ashu.animate({ height:"100px", opacity:"0.7"}, "slow")
-        ashu.animate({ width:"100px", opacity:"0.9"}, "slow")
+    // $("button").click(function () {
+    //     var ashu = $("div")
+    //     ashu.animate({ height:"300px", opacity:"0.4"}, "slow")
+    //     ashu.animate({ width:"300px", opacity:"0.5"}, "slow")
+    //     ashu.animate({ height:"100px", opacity:"0.7"}, "slow")
+    //     ashu.animate({ width:"100px", opacity:"0.9"}, "slow")
+    // })
+
+    $("button").click(function(){
+        var himu = $("div")
+
+        himu.animate({left: "300px"}, "slow")
+        himu.animate({width: "300px"}, "slow")
+        himu.animate({fontSize: "50px"}, "slow")
     })
 
 }) 
