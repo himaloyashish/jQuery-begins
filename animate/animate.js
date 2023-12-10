d6998235ee922cf5c8eae3bb0fc312a926a04990
himaloyash <himaloyashis98@gmail.com>
2023-12-10 22:52:10 +0600

    animation almost done

--- animate/animate.js
+++ animate/animate.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("div").animation({left: "12rem"})
+$(document).ready(function () {
+
+    $("button").click(function () {
+        $("div").animate({
+            left: "200px",
+            width:'150px',
+            height: '150px',
+            
+        })
     })
-})
+
+}) 
