4394e538260f801332e8e65b342615fea72192b7
himaloyash <himaloyashis98@gmail.com>
2023-12-13 11:52:38 +0600

    get method is almost done

---
+++ get/get.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    // for getting text
+
+    // $("#text").click(function(){
+    //     alert("text:" + $("p").text())
+    // })
+
+    // for getting html with markup
+
+    // $("#html").click(function(){
+    //     alert("text:" + $("p").html())
+    // })
+
+    
+})
